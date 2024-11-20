b260a350439c67444f6bafc079ab650eb91abefb
Alexander <zhelezkin.sasha@yandex.ru>
2024-11-20 23:00:16 +0300

    thirdCommit

--- src/main/java/com/chein/task2/DrawArc.java
+++ src/main/java/com/chein/task2/DrawArc.java
@@ -8,9 +8,13 @@ import javafx.scene.paint.Color;
 public class DrawArc {
     public static void drawArc(final GraphicsContext graphicsContext, final int xc, final int yc,
                                final int radius, final Color startColor, final Color endColor, double startAng, double endAng) {
-        final PixelWriter pixelWriter = graphicsContext.getPixelWriter();
         startAng = Math.toRadians(startAng);
         endAng = Math.toRadians(endAng);
+
+        if (startAng<0){
+            startAng = Math.toRadians(360) + startAng;
+            endAng = Math.toRadians(360) + endAng;
+        }
 
         int x = 0;
         int y = radius;
@@ -28,63 +32,46 @@ public class DrawArc {
                 x++;
             }
 
-            double ang1 = Math.atan2(x, y);
-            double ang2 = Math.atan2(y, x);
-            double ang3 = Math.atan2(y, -x);
-            double ang4 = Math.atan2(x, -y);
-            double ang5 = Math.atan2(-x, -y) + 2 * Math.PI;
-            double ang6 = Math.atan2(-y, -x) + 2 * Math.PI;
-            double ang7 = Math.atan2(-y, x) + 2 * Math.PI;
-            double ang8 = Math.atan2(-x, y) + 2 * Math.PI;
+            double ang1 = perevodUgla(Math.atan2(x, y), startAng);
+            double ang2 = perevodUgla(Math.atan2(y, x), startAng);
+            double ang3 = perevodUgla(Math.atan2(y, -x), startAng);
+            double ang4 = perevodUgla(Math.atan2(x, -y), startAng);
+            double ang5 = perevodUgla(Math.atan2(-x, -y) + 2 * Math.PI, startAng);
+            double ang6 = perevodUgla(Math.atan2(-y, -x) + 2 * Math.PI, startAng);
+            double ang7 = perevodUgla(Math.atan2(-y, x) + 2 * Math.PI, startAng);
+            double ang8 = perevodUgla(Math.atan2(-x, y) + 2 * Math.PI, startAng);
 
-            double moveEndAng = endAng;
             if (endAng < startAng) {
-                moveEndAng += 2 * Math.PI;
+                endAng += 2 * Math.PI;
             }
 
-            if (ang1 >= startAng && ang1 <= endAng) { // первый окнтант
-                double fraction = (ang1 - startAng) / (endAng - startAng); // отношение текущей длины ко всей
-                Color color = interpolation(startColor, endColor, fraction);
-                pixelWriter.setColor(xc + y, yc + x, color);
-            }
-            if (ang2 >= startAng && ang2 <= endAng) { // второй октант
-                double fraction = (ang2 - startAng) / (endAng - startAng);
-                Color color = interpolation(startColor, endColor, fraction);
-                pixelWriter.setColor(xc + x, yc + y, color);
-            }
-            if (ang3 >= startAng && ang3 <= endAng) { // третий октант
-                double fraction = (ang3 - startAng) / (endAng - startAng);
-                Color color = interpolation(startColor, endColor, fraction);
-                pixelWriter.setColor(xc - x, yc + y, color);
-            }
-            if (ang4 >= startAng && ang4 <= endAng) { // четвертый октант
-                double fraction = (ang4 - startAng) / (endAng - startAng);
-                Color color = interpolation(startColor, endColor, fraction);
-                pixelWriter.setColor(xc - y, yc + x, color);
-            }
+            draw(ang1, startAng, endAng, startColor, endColor, xc + y, yc + x, graphicsContext);
+            draw(ang2, startAng, endAng, startColor, endColor, xc + x, yc + y, graphicsContext);
+            draw(ang3, startAng, endAng, startColor, endColor, xc - x, yc + y, graphicsContext);
+            draw(ang4, startAng, endAng, startColor, endColor, xc - y, yc + x, graphicsContext);
+            draw(ang5, startAng, endAng, startColor, endColor, xc - y, yc - x, graphicsContext);
+            draw(ang6, startAng, endAng, startColor, endColor, xc - x, yc - y, graphicsContext);
+            draw(ang7, startAng, endAng, startColor, endColor, xc + x, yc - y, graphicsContext);
+            draw(ang8, startAng, endAng, startColor, endColor, xc + y, yc - x, graphicsContext);
 
-            // здесь обработка случая когда конечный угол больше 180
-            if (ang5 >= startAng && ang5 <= moveEndAng) { //пятый октант
-                double fraction = (ang5 - startAng) / (endAng - startAng);
-                Color color = interpolation(startColor, endColor, fraction);
-                pixelWriter.setColor(xc - y, yc - x, color);
-            }
-            if (ang6 >= startAng && ang6 <= moveEndAng) { //шестой октант
-                double fraction = (ang6 - startAng) / (endAng - startAng);
-                Color color = interpolation(startColor, endColor, fraction);
-                pixelWriter.setColor(xc - x, yc - y, color);
-            }
-            if (ang7 >= startAng && ang7 <= moveEndAng) { //седьмой октант
-                double fraction = (ang7 - startAng) / (endAng - startAng);
-                Color color = interpolation(startColor, endColor, fraction);
-                pixelWriter.setColor(xc + x, yc - y, color);
-            }
-            if (ang8 >= startAng && ang8 <= moveEndAng) { //восьмой октант
-                double fraction = (ang8 - startAng) / (endAng - startAng);
-                Color color = interpolation(startColor, endColor, fraction);
-                pixelWriter.setColor(xc + y, yc - x, color);
-            }
         }
+    }
+
+    private static void draw(double ang, double startAng, double endAng, Color startColor, Color endColor,
+                             int x, int y, final GraphicsContext graphicsContext){
+        final PixelWriter pixelWriter = graphicsContext.getPixelWriter();
+        if (ang >= startAng && ang <= endAng) {
+            double fraction = (ang - startAng) / (endAng - startAng);
+            Color color = interpolation(startColor, endColor, fraction);
+            pixelWriter.setColor(x, y, color);
+        }
+    }
+
+    private static double perevodUgla(double ang, double startAng){
+        if (ang < startAng){
+            ang += 2*Math.PI;
+        }
+        return ang;
     }
 
     public static Color interpolation(Color startColor, Color endColor, double fraction) {
@@ -97,3 +84,5 @@ public class DrawArc {
 
 
 }
+
+
